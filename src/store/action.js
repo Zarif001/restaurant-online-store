aa30367a4d7982d6664a17f1af145eb144d5f141
zarif <zarif@icloud.com>
2024-09-11 21:12:34 +0500

    test commit

--- src/store/action.js
+++ src/store/action.js
@@ -4,6 +4,7 @@ import { productActions } from "./index";
 export const fetchDates = () => {
   return async (dispatch) => {
     dispatch(productActions.fetchPending());
+
     const getData = async () => {
       const response = await fetch('https://restaurant-6b45b-default-rtdb.firebaseio.com/data.json');
       if (!response.ok) {
@@ -12,10 +13,28 @@ export const fetchDates = () => {
       return await response.json();
     };
 
+    const loadImages = (imageUrls) => {
+      return Promise.all(
+        imageUrls.map((url) => {
+          return new Promise((resolve, reject) => {
+            const img = new Image();
+            img.src = url;
+            img.onload = resolve;  // Успешная загрузка
+            img.onerror = reject;  // Ошибка при загрузке
+          });
+        })
+      );
+    };
+
     try {
       const data = await getData();
-      dispatch(productActions.fetchSuccess()); // Устанавливаем статус успеха
       dispatch(productActions.fetchDataGet(data));
+
+      const imageUrls = data.map(item => item.imageUrl); // Пример, если в каждом объекте есть поле imageUrl
+
+      await loadImages(imageUrls);
+
+      dispatch(productActions.fetchSuccess()); // Устанавливаем статус успеха
     } catch (error) {
       dispatch(productActions.fetchError()); // Устанавливаем статус ошибки
       console.error('Error:', error);
