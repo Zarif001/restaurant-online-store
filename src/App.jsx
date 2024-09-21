59339dcc73b271a77d1675e760b141e559b4dab9
zarif <zarif@icloud.com>
2024-09-21 22:51:15 +0500

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,39 @@
-// App.js
-import React, { useEffect } from "react";
+import React, { Suspense, lazy, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Main from "./pages/MainPage/MainPage";
-import MenuPage from "./pages/MenuPage/MenuPage";
 import CartPage from "./pages/CartPage/CartPage";
 import { fetchDates, cartFetchDates, putFetchingData } from "./store/action";
 import { ThreeCircles } from "react-loader-spinner";
 import AboutPage from "./pages/AboutPage/AboutPage";
-import GalleryPage from "./pages/GalleryPage/GalleryPage";
 import ContactPage from "./pages/ContactPage/ContactPage";
 
+const MenuPage = lazy(() => import('./pages/MenuPage/MenuPage'));
+const GalleryPage = lazy(() => import('./pages/GalleryPage/GalleryPage'));
+
 const router = createBrowserRouter([
-  {path: '/', element: <Main/>},
-  {path: '/menu', element: <MenuPage/>},
-  {path: '/cart', element: <CartPage/>},
-  {path: '/about', element: <AboutPage/>},
-  {path: '/gallery', element: <GalleryPage/>},
-  {path: '/contact', element: <ContactPage/>}
-])
+  { path: '/', element: <Main /> },
+  { path: '/cart', element: <CartPage /> },
+  { path: '/about', element: <AboutPage /> },
+  { path: '/contact', element: <ContactPage /> },
+  {
+    path: '/menu',
+    element: (
+      <Suspense fallback={<p>...</p>}>
+        <MenuPage />
+      </Suspense>
+    ),
+  },
+  {
+    path: '/gallery',
+    element: (
+      <Suspense fallback={<p>...</p>}>
+        <GalleryPage />
+      </Suspense>
+    ),
+  },
+]);
+
 function App() {
   const dispatch = useDispatch();
   const cartItems = useSelector((state) => state.products.cartItems);
@@ -35,7 +50,7 @@ function App() {
     }
   }, [cartItems, dispatch]);
 
-  if (status === "loading")
+  if (status === "loading") {
     return (
       <div className="loader">
         <ThreeCircles
@@ -47,9 +62,9 @@ function App() {
         />
       </div>
     );
-  return (
-    <RouterProvider router={router}/>
-  );
+  }
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
